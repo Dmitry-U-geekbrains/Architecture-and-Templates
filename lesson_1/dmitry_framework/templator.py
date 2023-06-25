7568311763ae3ab7f5a416825a00cbd1c58263c7
User <ushakov.ip@yandex.ru>
2023-06-25 20:00:55 +0300

    lesson_1

--- lesson_1/dmitry_framework/templator.py
+++ lesson_1/dmitry_framework/templator.py
@@ -2,14 +2,14 @@ from jinja2 import Template
 from os.path import join
 
 
-def render(template_name, folder='templates', **kwargs):
+def render(simplestyle_banner, folder='templates', **kwargs):
     """
     :param template_name: имя шаблона
     :param folder: папка в которой ищем шаблон
     :param kwargs: параметры
     :return:
     """
-    file_path = join(folder, template_name)
+    file_path = join(folder, simplestyle_banner)
     # Открываем шаблон по имени
     with open(file_path, encoding='utf-8') as f:
         # Читаем
